d2e93cdfb2bdadffd3072f15bd6aa16d2c0049d0
admin <2199526021@qq.com>
2020-08-06 07:32:03 +0800

    one

---
+++ src/main/java/com/huoshaoyun/entity/Person.java
@@ -0,0 +1,11 @@
+package com.huoshaoyun.entity;
+
+/**
+ * @Author: DynamicComp
+ * @CreateTime: 2020-08-06 07:21:14
+ * @Version: 1.0
+ */
+public class Person{
+	private String name;
+	private Integer age;
+}
