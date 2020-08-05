16bf9b897bef07cc0776af8ad2641bb2f1af12a1
admin <2199526021@qq.com>
2020-08-06 07:57:10 +0800

    one

---
+++ src/main/java/com/huoshaoyun/entity/Man.java
@@ -0,0 +1,9 @@
+package com.huoshaoyun.entity;
+
+/**
+ * @Author: DynamicComp
+ * @CreateTime: 2020-08-06 07:37:36
+ * @Version: 1.0
+ */
+public class Man{
+}
---
+++ src/main/java/com/huoshaoyun/entity/Teacher.java
@@ -0,0 +1,9 @@
+package com.huoshaoyun.entity;
+
+/**
+ * @Author: DynamicComp
+ * @CreateTime: 2020-08-06 07:36:59
+ * @Version: 1.0
+ */
+public class Teacher{
+}
